5f86efaab291cf76a3c6fd3cd59b56c26b7acf1f
Kevin Kamto <kamtokevin@gmail.com>
2020-03-02 16:59:41 -0500

    tweaks: added intents for individual users

--- __init__.py
+++ __init__.py
@@ -8,13 +8,29 @@ class HackerMan(MycroftSkill):
     @intent_file_handler('man.hacker.intent')
     def handle_man_hacker(self, message):
         self.speak_dialog('man.hacker')
-        subprocess.Popen(["sudo", "pwd"])
+        # subprocess.Popen(["sudo", "pwd"])
         subprocess.Popen (["sudo", "python3", "/opt/mycroft/skills/hacker-man-skill/blink.py"])
     
-    @intent_file_handler('man.hacker.stop.intent')
+    @intent_file_handler('man.hacker.stop.intent') 
     def handle_man_hacker_stop(self, message):
         self.speak_dialog('man.hacker.stop')
-        subprocess.Popen(["sudo", "pwd"])
+        # subprocess.Popen(["sudo", "pwd"]) #Testing of working subprocess
+        subprocess.Popen (["sudo", "python3", "/opt/mycroft/skills/hacker-man-skill/blink.py"])
+
+    @intent_file_handler('man.aj.intent') 
+    def handle_alcinder(self, message):
+        subprocess.Popen (["sudo", "python3", "/opt/mycroft/skills/hacker-man-skill/blink.py"])
+
+    @intent_file_handler('man.bryan.intent') 
+    def handle_bryan(self, message):
+        subprocess.Popen (["sudo", "python3", "/opt/mycroft/skills/hacker-man-skill/blink.py"])
+
+    @intent_file_handler('man.joe.intent') 
+    def handle_joe(self, message):
+        subprocess.Popen (["sudo", "python3", "/opt/mycroft/skills/hacker-man-skill/blink.py"])
+
+    @intent_file_handler('man.aj.intent') 
+    def handle_kevin(self, message):
         subprocess.Popen (["sudo", "python3", "/opt/mycroft/skills/hacker-man-skill/blink.py"])
 
 
